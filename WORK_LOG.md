c7138124613b0f0817b12c829eb8e758b6e0df71
Alexandre Haddad-Delaveau <ahaddaddelaveau@gmail.com>
2025-02-27 20:46:15 -0800

    See Work Log

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -3,6 +3,7 @@ Name: Alexandre Haddad-Delaveau
 | Date   |    Time     |                                                                                                                                                                                                                                          Update |
 |:-------|:-----------:|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
 | Feb 27 | 19:00-19:45 | Completed first solution using memoization and recursion. I REALLY want to do it with tabulation, but unfortunately I have not really thought of a way to do so in class... so I decided to focus on getting a memoization solution done first. |
+| Feb 27 | 19:45-20:45 |                                                                                                                                                Came up with a solution using tabulation. It isn't particularly space efficient... but it works! |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -17,40 +17,24 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
-        // Initialize memoization table
-        int[][] memo = new int[doc1.length() + 1][doc2.length() + 1];
-        for (int i = 0; i <= doc1.length(); i++) {
-            Arrays.fill(memo[i], -1);
+        // Create 2D array to store tabulation results
+        int[][] results = new int[doc1.length() + 1][doc2.length() + 1];
+
+        // Iterate through all characters (in both docs)
+        for (int x = 1; x <= doc1.length(); x++) {
+            for (int y = 1; y <= doc2.length(); y++) {
+                // Check if characters at current indices are equal
+                if (doc1.charAt(x - 1) == doc2.charAt(y - 1)) {
+                    // If they are equal, increment the previous result by 1
+                    results[x][y] = results[x - 1][y - 1] + 1;
+                } else {
+                    // Otherwise, skip a character from either document, and find the max of the two
+                    results[x][y] = Math.max(results[x - 1][y], results[x][y - 1]);
+                }
+            }
         }
 
-        // Start recursing
-        return longestSharedSubstring(doc1, doc2, 0, 0, memo);
-    }
-
-    private static int longestSharedSubstring(String doc1, String doc2, int index1, int index2, int[][] memo) {
-        // Base case: Substring must be 0 if we've reached the end of either document
-        if (index1 == doc1.length() || index2 == doc2.length()) {
-            return 0;
-        }
-
-        // Memoization: check for cached value
-        if (memo[index1][index2] != -1) {
-            return memo[index1][index2];
-        }
-
-        // Check if characters match
-        if (doc1.charAt(index1) == doc2.charAt(index2)) {
-            // If they match, add 1 and continue
-            memo[index1][index2] = 1 + longestSharedSubstring(doc1, doc2, index1 + 1, index2 + 1, memo);
-        } else {
-            // Otherwise, find the maximum of incrementing each index
-            memo[index1][index2] = Math.max(
-                    longestSharedSubstring(doc1, doc2, index1 + 1, index2, memo),
-                    longestSharedSubstring(doc1, doc2, index1, index2 + 1, memo)
-                    );
-        }
-
-        // Return result
-        return memo[index1][index2];
+        // Return the length of the longest shared substring
+        return results[doc1.length()][doc2.length()];
     }
 }
